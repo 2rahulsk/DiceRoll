6345e8b1cb712dfddba944f2cb01d6cff35613e0
2rahulsk <30774426+2rak@users.noreply.github.com>
2019-02-06 23:32:11 +1100

    Initial Commit

---
+++ DiceRoll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceRoll
+//
+//  Created by Rahul Krishnan on 6/2/19.
+//  Copyright © 2019 Rahul Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
